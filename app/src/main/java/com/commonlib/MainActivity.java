a0356248aa268ac4adf342a0efb84f462d974f39
1024-1024 <zhangwl.freedom@gmail.com>
2017-10-30 23:21:42 +0800

    Klog

--- app/src/main/java/com/commonlib/MainActivity.java
+++ app/src/main/java/com/commonlib/MainActivity.java
@@ -3,7 +3,7 @@ package com.commonlib;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.commonlog.CLog;
+import com.commonlog.Q.CLog;
 
 public class MainActivity extends AppCompatActivity {
 
--- clog/src/main/java/com/commonlog/Q/CLog.java
+++ clog/src/main/java/com/commonlog/Q/CLog.java
@@ -1,4 +1,4 @@
-package com.commonlog;
+package com.commonlog.Q;
 
 import android.text.TextUtils;
 import android.util.Log;
